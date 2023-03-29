d397d5320cd2d61bffe2228a3618a2fb4c82c9b9
galAntonis <galant.dev@gmail.com>
2023-03-29 11:30:07 +0300

    main struct

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+import {Footer, Blog, Features, Header} from './containers';
+import {CTA, Brand, Navbar} from './components';
+
+const App = () => {
+  return (
+	<div className="App">
+		<div className="gradient__bg">
+			<Navbar/>
+			<Header/>
+		</div>
+		<Brand/>
+		<Features/>
+		<CTA/>
+		<Blog/>
+		<Footer/>
+	</div>
+  )
+}
+
+export default App
